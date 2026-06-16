2e1744d9a5c9c781cb6e5dc6a852eaf6551e8e49
Nithish Baba <nitbaba@gmail.com>
2026-06-16 17:17:06 -0400

    small fixes for clarity and no longer console

--- src/jobs/stream_to_bronze.py
+++ src/jobs/stream_to_bronze.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType
 from pyspark.sql.functions import current_timestamp
@@ -9,19 +10,21 @@ def main():
     project_root = os.path.abspath(os.path.join(script_dir, "../../"))
 
     landing_dir = os.path.join(project_root, "data_lake", "landing")
+    bronze_dir = os.path.join(project_root, "data_lake", "bronze")
     checkpoint_dir = os.path.join(project_root, "data_lake", "checkpoints", "bronze")
 
-    print("Initializing local Spark session")
+    print("\nInitializing local Spark session\n")
 
     spark = (SparkSession.builder
              .appName("CustomerFeedbackBronzeStraming")
              .master("local[4]")
+             .config("spark.pyspark.driver.acceptConnIfUntrusted", "true")
              .getOrCreate())
 
     #reduce logging
     spark.sparkContext.setLogLevel("WARN")
 
-    print("Defining expected incoming Data scheme")
+    print("\nDefining expected incoming Data scheme\n")
 
     schema = StructType([
         StructField("ticket_id", StringType(), True),
@@ -31,7 +34,7 @@ def main():
         StructField("timestamp", StringType(), True)
     ])
 
-    print(f"Monitor Landing Zone: {landing_dir}")
+    print(f"\nMonitor Landing Zone: {landing_dir}\n")
     #readStream, tells Spark to treat dir as live data source
     raw_stream_df = (spark.readStream
                      .schema(schema)
@@ -41,29 +44,33 @@ def main():
     #Prod pattern to note: adding timestamp to Bronze layer df
     bronze_enriched_df = raw_stream_df.withColumn("ingested_at", current_timestamp())
 
-    print("Streaming engine started. Waiting for data batches")
+    print(f"\nDirecting raw stream output to Bronze tier: {bronze_dir}\n")
+    print(f"\nStreaming engine started. Waiting for data batches\n")
     print(f"send sigkill(ctrl+c) to stop.\n")
 
     #writeStream, tells destination. For local test, goes to terminal
     query = (bronze_enriched_df.writeStream
-             .format("console")
+             .format("parquet")
              .outputMode("append")
              #checkpoint for data projress(file offets)
              .option("checkpointLocation", checkpoint_dir)
-             .start())
+             .start(bronze_dir))
 
     try:
         query.awaitTermination()
-    except KeyboardInterrupt:
-        print("\n Shutdown received. Stopping now.")
+    except (KeyboardInterrupt, Exception):
+        print("\n Shutdown received. Stopping now.\n")
 
-        #stops streaming thread from reading files
-        query.stop()
+        try:
+            #stops streaming thread from reading files
+            query.stop()
 
-        #stop spark engine cluster that was spun up
-        spark.stop()
+            #stop spark engine cluster that was spun up
+            spark.stop()
+        except Exception:
+            pass
 
-        print("Session stopped safely.")
+        print("\nSession stopped safely.")
         sys.exit(0)
 
 if __name__ == "__main__":
